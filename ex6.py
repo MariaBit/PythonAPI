54b5e2863fb30a185115a629712276c80a82ed51
98855 <50masha50>
2022-10-10 01:32:25 +0400

    ex 6

---
+++ ex6.py
@@ -0,0 +1,12 @@
+import requests
+
+response = requests.post("https://playground.learnqa.ru/api/long_redirect", allow_redirects=True)
+
+redirects_counter = len(response.history)
+
+print(f'Количество редиректов от изначальной точки назначения до итоговой для метода https://playground.learnqa.ru/api/long_redirect: {redirects_counter}')
+
+last_index = (redirects_counter - 1)
+last_redirect = response.history[last_index]
+
+print(f'Итоговый URL: {last_redirect.url}')
