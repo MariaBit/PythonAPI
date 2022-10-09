f51676b54f4f40f007097d75dbe585f7e8e73728
98855 <50masha50>
2022-10-10 01:36:51 +0400

    added if block

--- ex6.py
+++ ex6.py
@@ -6,7 +6,8 @@ redirects_counter = len(response.history)
 
 print(f'Количество редиректов от изначальной точки назначения до итоговой для метода https://playground.learnqa.ru/api/long_redirect: {redirects_counter}')
 
-last_index = (redirects_counter - 1)
-last_redirect = response.history[last_index]
+if redirects_counter > 0:
+    last_index = (redirects_counter - 1)
+    last_redirect = response.history[last_index]
 
-print(f'Итоговый URL: {last_redirect.url}')
+    print(f'Итоговый URL: {last_redirect.url}')
